0a31bd060b76f6073acecd2a30ab802e8cf219a4
Timothy Loftus <tiloftus.4@gmail.com>
2025-05-12 14:53:29 -0400

    changed core.py to have some functions

--- SICSS_Pkg/core.py
+++ SICSS_Pkg/core.py
@@ -1,2 +1,9 @@
-def shout(text):
-    return text.upper() + "!!!"
+def count_words(text):
+    """Count the number of words in a string."""
+    return len(text.split())
+
+def get_top_n_words(texts, n=5):
+    """Return the top n most common words in a list of texts."""
+    from collections import Counter
+    all_words = " ".join(texts).split()
+    return Counter(all_words).most_common(n)
